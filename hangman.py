0053fdd154a9fe09805e2f5ad34264e9e19b6290
JeanKim7 <jean.g.km@gmail.com>
2024-03-07 16:14:10 -0800

    finish hangman game

--- hangman.py
+++ hangman.py
@@ -5,19 +5,10 @@ import random
 
 def position_correct_letter(letter, word):
     indices = []
-    position = -1
     for i in range(len(word)):
         if letter == word[i]:
             indices.append(i)
     return indices
-
-def generate_word():
-    word_string = "hospital reinforce glove article recognize identity process bargain practical publication cooperative important prosper shatter bed yard game cable stock toss dish democratic thank impound jungle"
-    word_list = word_string.split()
-    answer = random.choice(word_list)
-
-    blank_list =["_" for a in range(len(answer))]
-    blank_template = "".join(blank_list)
 
 
 def hangman():
@@ -29,31 +20,36 @@ def hangman():
     blank_template = "".join(blank_list)
     
     tries = 8
+    attempts = 0
 
     used_letters = []
 
-    for i in range(1, 9):
+    for i in range(1, (8+len(answer))):
+        if tries == 0:
+            break
         print(blank_template)
         print(f"Tries left: {tries}")
         print(f"Letters used: {used_letters}")
         guess = input("Enter a guess for the letter: ").lower()
-        while (len(guess) != 1 and guess in used_letters):
+        while (guess.isdigit() == True or len(guess) != 1 or guess in used_letters):
             guess = input("Enter only ONE letter that you have NOT USED: ").lower()
         if guess not in answer:
             used_letters.append(guess)
             print("Sorry, that letter isn't in the word!")
-            tries -= 1
+            tries-=1
+            attempts+=1
         else:
             indices = position_correct_letter(guess, answer)
             for i in indices:
                 blank_list[i] = guess
-            tries -=1
+                blank_template = "".join(blank_list)
+                attempts+=1
             print("That letter is in the word!")
             if blank_template == answer:
-                print(f"Congratulations, you won in {tries} tries! The answer is {answer}.")
+                print(f"Congratulations, you won in {attempts} attempts! The answer is {answer}.")
                 break
 
-    print("Game Over! You ran out of tries")
+    else: print(f"Game Over! You ran out of tries. The answer is {answer}")
 
 
 
